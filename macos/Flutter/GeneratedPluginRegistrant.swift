c6fabfbd6507c9b1568f89d39b5bddf46bf07728
TimothyTanawi-231401079 <timothytanawi63@gmail.com>
2025-11-29 09:19:31 +0700

    feat: add VideoPlayer screen, integrate full navigation Record Video flow, and enable saving recorded videos to the gallery

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,21 +8,17 @@ import Foundation
 import cloud_firestore
 import firebase_auth
 import firebase_core
+import geolocator_apple
 import path_provider_foundation
-<<<<<<< HEAD
-import video_player_avfoundation
-=======
 import shared_preferences_foundation
->>>>>>> 4326058a883f24dc4fc48dba10ac13f0aaeeebf5
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-<<<<<<< HEAD
-  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
-=======
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
->>>>>>> 4326058a883f24dc4fc48dba10ac13f0aaeeebf5
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,10 +3,11 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  permission_handler_windows
   cloud_firestore
   firebase_auth
   firebase_core
+  geolocator_windows
+  permission_handler_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
